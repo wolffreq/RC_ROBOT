90aef78a35a5db1b53f65eab0c37d3df7625d8a4
Shane Miley <addshane@gmail.com>
2016-04-19 19:37:57 +0100

    working

--- robot.py
+++ robot.py
@@ -11,7 +11,7 @@ GPIO.setup(servoPin1, GPIO.OUT)
 GPIO.setup(servoPin2, GPIO.OUT)
 pwm1 = GPIO.PWM(servoPin1,50)
 pwm2 = GPIO.PWM(servoPin2,50)
-
+#gui
 root = Tk() 
 root.wm_title("Robot Control") 
 root.config(background = "#FFFFFF")
